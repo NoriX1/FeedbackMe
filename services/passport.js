ddf1feca589286ae3936cf36f29d59576ecd6140
NoriX <versatility.95@mail.ru>
2019-03-24 18:04:40 +0300

    Creating a Query and Saving Instances into MongoDB

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy(
     {
@@ -9,6 +12,16 @@ passport.use(new GoogleStrategy(
         callbackURL: '/auth/google/callback'
     },
     (accesToken, refreshToken, profile, done) => {
-        console.log(profile);
+        User.findOne({ googleId: profile.id }).then((existingUser) => {
+            if (existingUser) {
+                // we already have a record with the given profile ID
+                done(null, existingUser);
+            } else {
+                // we don`t have a user record with this ID, make a new record
+                new User({ googleId: profile.id })
+                    .save()
+                    .then(user => done(null, user));
+            }
+        })
     })
 );
