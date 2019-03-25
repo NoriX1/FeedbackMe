d034f8917a827f020f2be026d0bb8c8297117cd0
NoriX <versatility.95@mail.ru>
2019-03-25 19:03:36 +0300

    Empty components, Routing, Header Component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import Header from './Header';
+import Dashboard from './Dashboard';
+import SurveyNew from './SurveyNew';
+import Landing from './Landing';
+import { fetchUser } from '../actions';
 
 class App extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
     render() {
-        return <a href="/auth/google">Sign in With Google</a>
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing}></Route>
+                        <Route exact path="/surveys" component={Dashboard}></Route>
+                        <Route path="/surveys/new" component={SurveyNew}></Route>
+                        <Route></Route>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
     }
 }
 
-export default App;
+export default connect(null, { fetchUser })(App);
