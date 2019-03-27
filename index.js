337cce91ee6cded0dfbb8aa954dc3e927d931de8
NoriX <versatility.95@mail.ru>
2019-03-27 16:10:03 +0300

    Completing a Charge on backend side

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const keys = require('./config/keys')
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -19,6 +21,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
