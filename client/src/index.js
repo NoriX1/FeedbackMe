50b4a671eeff0ed7aa8c8f9172dab079ba46cf74
NoriX <versatility.95@mail.ru>
2019-03-28 21:38:09 +0300

    new message template, saving survey to DB, responding

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,9 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
+//Next 2 lines used for testing backend from browser
+import axios from 'axios';
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
