bf7546406ad5d20f2723d289ac7fcfcdbe50b22f
NoriX <versatility.95@mail.ru>
2019-03-29 17:05:05 +0300

    SurveyFormReview component

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -5,13 +5,7 @@ import { Link } from 'react-router-dom';
 
 import SurveyField from './SurveyField';
 import validateEmails from '../../utils/validateEmails';
-
-const FIELDS = [
-    { label: 'Survey Title', name: 'title' },
-    { label: 'Subject Line', name: 'subject' },
-    { label: 'Email Body', name: 'body' },
-    { label: 'Recipient List', name: 'emails' },
-];
+import FIELDS from './formFields';
 
 class SurveyForm extends React.Component {
 
@@ -24,7 +18,7 @@ class SurveyForm extends React.Component {
     render() {
         return (
             <div>
-                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
                     {this.renderFields()}
                     <Link to="/surveys" className="red btn-flat white-text">Cancel</Link>
                     <button type="submit" className="teal btn-flat right white-text">
@@ -52,5 +46,6 @@ function validate(values) {
 
 export default reduxForm({
     form: 'surveyForm',
-    validate
+    validate,
+    destroyOnUnmount: false
 })(SurveyForm);
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
+
 class SurveyNew extends React.Component {
+    state = { showFormReview: false };
+
+    renderContent() {
+        if (this.state.showFormReview) {
+            return <SurveyFormReview onCancel={() => this.setState({ showFormReview: false })} />
+        }
+
+        return <SurveyForm
+            onSurveySubmit={() => this.setState({ showFormReview: true })}
+        />
+    }
+
     render() {
         return (
             <div>
-                <h5>Creating New Survey</h5>
-                <SurveyForm />
+                {this.renderContent()}
             </div>
         );
     }
