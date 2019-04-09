9e18a131894e82aeb00d7f9b22650bf0bdeb661d
NoriX <versatility.95@mail.ru>
2019-04-09 16:15:42 +0300

    Yandex payments in FrontEnd

---
+++ client/src/components/payments/PaymentFail.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PaymentFail = () => {
+    return (
+        <h5>Payment Declined, please, try again!</h5>
+    );
+}
+
+export default PaymentFail;
---
+++ client/src/components/payments/PaymentSuccess.js
@@ -0,0 +1,23 @@
+import React from "react";
+import * as actions from '../../actions';
+import { connect } from 'react-redux';
+
+class PaymentSuccess extends React.Component {
+    state = { time: 5 }
+    componentDidMount() {
+        setTimeout(() => {
+            this.props.fetchUser();
+            this.props.history.push('/surveys')
+        }, 5000);
+        this.interval = setInterval(() => { this.setState({ time: this.state.time - 1 }) }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+    render() {
+        return <h5>Payment Success! Redirecting to the homepage throught {this.state.time} seconds.</h5>
+    }
+}
+
+export default connect(null, actions)(PaymentSuccess);
