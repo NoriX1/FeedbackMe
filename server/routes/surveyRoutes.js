a98a234666b3125e2763bf2aa1ce7c600703e84b
NoriX <versatility.95@mail.ru>
2020-09-17 20:09:23 +0300

    Survey template edited

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -74,12 +74,13 @@ module.exports = app => {
   });
 
   app.post('/api/surveys', requireAuth, requireCredits, async (req, res) => {
-    const { title, subject, body, recipients } = req.body;
+    const { title, subject, body, recipients, description = '' } = req.body;
 
     const survey = new Survey({
       title,
       subject,
       body,
+      description,
       recipients: recipients.split(',').map(email => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now()
